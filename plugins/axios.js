e13f0d603fc319b68a04360d03a9333a2e5ab43b
Bui Van Anh <buivananh01072003@gmail.com>
2023-09-28 16:18:41 +0700

    fix

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default defineNuxtPlugin((NuxtApp) => {
 
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'http://localhost:4000/api'
+    axios.defaults.baseURL = 'https://tiktok-clone-api-701l.onrender.com/api'
     return {
         provide: { 
             axios: axios
